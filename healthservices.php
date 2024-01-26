81e93db18248a1f61b95baa0c356542242d08c27
dmaelarss <153101789+dmaelarss@users.noreply.github.com>
2024-01-26 15:42:30 +0800

    Add files via upload

---
+++ healthservices.php
@@ -0,0 +1,19 @@
+<?php
+    include("header.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit a cupiditate rerum, fuga, officia nisi fugiat consectetur, pariatur sapiente quod obcaecati reprehenderit? Unde deserunt sequi ipsum dolore fugit minima numquam!<br>
+    Stuff about your home page can go here<br>
+</body>
+</html>
+
+<?php
+    include("footer.html");
+?>
---
+++ index.php
@@ -0,0 +1,213 @@
+<?php
+    include("header.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <script src="https://kit.fontawesome.com/c6e3a86e35.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="home-container1">
+    <img src="images/logo.png" class="logo">
+    <h3>TOWNTECH INNOVATIONS</h3>
+    <p>HEALTH SERVICES AND SOCIAL WELFARE SYSTEM</p>
+</div>
+
+    <div class="slider">
+        <figure>
+            <div class="slide">
+                <a href="healthservices.php"><h1>Health Topics</h1></a>
+                <img src="images/logo4.png">
+            </div>
+            <div class="slide">
+                <a href="#"><h1>Social Welfare</h1></a>
+                <img src="images/logo3.png">
+            </div>
+            <div class="slide">
+                <a href="#"><h1>News and Updates</h1></a>
+                <img src="images/logo2.png">
+            </div>
+            <div class="slide">
+                <a href="#"><h1>Social Welfare</h1></a>
+                <img src="images/logo4.png">
+            </div>
+        </figure>
+    </div>
+    <div class="home-container2">
+      <h1>Our Key Features</h1>
+      <p>Discover the key features of our health services and social
+          welfare system</p>
+      <div class="row">
+        <div class="services">
+          <h2>Healthcare Services</h2>
+          <p>Access to a wide range of healthcare services including medical consultations, diagnostic test, and treatments.</p>
+        </div>
+        <div class="services">
+          <h2>Social Welfare Programs</h2>
+          <p>Support for individuals and families through various social welfare
+              programs such as financial assistance, housing support, and employment training.</p>
+        </div>
+        <div class="services">
+          <h2>Community Outreach Initiatives</h2>
+          <p>We actively engage with the community through outreach Initiatives
+              to promote health care and social welfare awareness.</p>
+        </div>
+        <div class="services">
+          <h2>Collaboration with Local Organization</h2>
+          <p>We collaborate with local organizations to enhance the effectiveness of our health care and social welfare assistance programs.</p>
+        </div>
+      </div>
+    </div>
+    <div class="main3">
+    <div class="home-container3">
+        <h1 class="section-header">Events</h1>
+        <div class="main-content">
+            <div class="box">
+                <img src="images/events1.png" alt="">
+                <div class="img-text">
+                    <div class="content">
+                        <h2>Barangay Job Fair</h2>
+                        <p>Mini Job Fair</p>
+                    </div>
+                </div>
+            </div>
+            <div class="box">
+                <img src="images/events2.png" alt="">
+                <div class="img-text">
+                    <div class="content">
+                        <h2>Cash Aid Distribution</h2>
+                        <p>Cash assistance for affected individual and families</p>
+                    </div>
+                </div>
+            </div>
+            <div class="box">
+                <img src="images/events3.png" alt="">
+                <div class="img-text">
+                    <div class="content">
+                        <h2>Technology Demonstration</h2>
+                        <p>Livelihood Program</p>
+                    </div>
+                </div>
+            </div>
+            <div class="box">
+                <img src="images/events4.png" alt="">
+                <div class="img-text">
+                    <div class="content">
+                        <h2>Annual Medical Mission</h2>
+                        <p>Free Checkup, Medication, and Medical Consultation</p>
+                    </div>
+                </div>
+            </div>
+            <div class="box">
+                <img src="images/events5.png" alt="">
+                <div class="img-text">
+                    <div class="content">
+                        <h2>Bring Back Smile Care</h2>
+                        <p>Dental Mission that is free for all</p>
+                    </div>
+                </div>
+            </div>
+            <div class="box">
+                <img src="images/events6.png" alt="">
+                <div class="img-text">
+                    <div class="content">
+                        <h2>iCare <br>Medical Mission</h2>
+                        <p>Free Eye Consultation and Prescription Glasses</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <div class="home-faq">
+      <div class="faqContainer">
+        <div class="home-faq1">
+          <div class="home-container29">
+            <span class="overline">
+              <span>FAQ</span>
+              <br />
+            </span>
+            <h2 class="home-text85 heading2">Common questions</h2>
+            <span class="home-text86 bodyLarge">
+              <span>
+                Here are some of the most common questions that we get.
+              </span>
+              <br />
+            </span>
+          </div>
+          <div class="home-container30">
+            <div class="question1-container">
+              <span class="question1-text heading3">
+                <h4>What is health care assistance?</h4>
+              </span>
+              <span class="bodySmall">
+                <span>
+                  Health care assistance refers to the provision of medical services and support to 
+                  individuals who require medical attention or treatment.
+                </span>
+              </span>
+            </div>
+            <div class="question1-container">
+              <span class="question1-text heading3">
+                <h4>What is social welfare assistance?</h4>
+              </span>
+              <span class="bodySmall">
+                <span>
+                  Social welfare assistance refers to the support and aid provided to individuals or 
+                  families in need, including financial assistance, housing support, and access to 
+                  social services.
+                </span>
+              </span>
+            </div>
+            <div class="question1-container">
+              <span class="question1-text heading3">
+                <h4>How can I access your services?</h4>
+              </span>
+              <span class="bodySmall">
+                <span>
+                To access our services, you can visit our website and fill out the contact form or call
+                our helpline for assistance.
+                </span>
+              </span>
+            </div>
+            <div class="question1-container">
+              <span class="question1-text heading3">
+                <h4>How can I apply for health services and social welfare benefits?</h4>
+              </span>
+              <span class="bodySmall">
+                <span>
+                  To apply for health services and social welfare benefits, you can visit your local government
+                  office or access online application portals. The specific application process may vary depending 
+                  on the services or program you are applying for.
+                </span>
+              </span>
+            </div>
+            <div class="question1-container">
+              <span class="question1-text heading3">
+                <h4>
+                  What documents do I need to provide when applying for health 
+                  services and social welfare benefits?
+                </h4>
+              </span>
+              <span class="bodySmall">
+                <span>
+                  The required documents may vary depending on the service or program you are applying
+                  for. Generally, you will need to provide identification documents, proof of income,
+                  residency documents, and any relevant medical or disability-related documents.
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</body>
+</html>
+
+<?php
+    include("footer.html");
+?>
